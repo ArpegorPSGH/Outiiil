a5fa73eda564020f6e73ec22bf4cf75d728a1aeb
ArpegorPSGH <greauhamard.pierresamuel@gmail.com>
2026-09-26 18:45:56 +0200

    work commit

--- js/background.js
+++ js/background.js
@@ -1,22 +1,22 @@
-function executeCode(code) {
-    eval(code);
-}
-
 function setImages(blobs) {
     window.OUTIIIL_DYNAMIC_IMAGES = blobs;
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === 'EXECUTE_SCRIPT') {
-        const funcMap = {
-            'executeCode': executeCode,
-            'setImages': setImages
-        };
-        chrome.scripting.executeScript({
-            target: { tabId: sender.tab.id },
-            func: funcMap[message.funcName],
-            args: message.args
-        }).then(() => sendResponse(true)).catch(sendResponse);
+        if (message.funcName === 'executeCode') {
+            // Use code parameter instead of func to avoid CSP eval violation
+            chrome.scripting.executeScript({
+                target: { tabId: sender.tab.id },
+                code: message.args[0]
+            }).then(() => sendResponse(true)).catch(sendResponse);
+        } else if (message.funcName === 'setImages') {
+            chrome.scripting.executeScript({
+                target: { tabId: sender.tab.id },
+                func: setImages,
+                args: message.args
+            }).then(() => sendResponse(true)).catch(sendResponse);
+        }
         return true;
     }
 });
